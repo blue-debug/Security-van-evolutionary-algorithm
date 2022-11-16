f8889bfcdade6945bb7b2a314655e3c2d2963f1e
blue-debug <blueybf777@outlook.com>
2022-11-16 18:08:38 +0800

    add comnents

--- cross.py
+++ cross.py
@@ -20,7 +20,7 @@ while i < len(data):
     if (data[i].find("bag") == 1):
         dict[int(re.findall("\d+", data[i])[0], 10) - 1] = [float(re.findall(r"[-+]?\d*\.\d+|\d+", data[i+1])[0]), float(re.findall(r"[-+]?\d*\.\d+|\d+", data[i+2])[0])]
     i += 1  
-    ## convert txt to dict with float
+    ## use regular exgression to  convert txt to dict with float
 
 for times in range(p): ## inital
     temp, sum = [], 0
@@ -29,10 +29,10 @@ for times in range(p): ## inital
         if (now_key not in temp):
             temp.append(now_key)
             sum += dict[now_key][0]
-            if (sum > tol_weight):
+            if (sum > tol_weight): ## add items until weight more than 285
                 temp.pop()
                 sum -= dict[now_key][0]
-                break
+                break  
     population[times] = temp
 print(population)
 
@@ -42,6 +42,7 @@ def crossover(parent_l, parent_r):
     # f = p2[0 : index] + p1[index : len(p1)]
     # res = parent_l[0 : index] + parent_r[index : len(parent_r)]
 
+    # random single point crossover
     index = random.randint(1, len(parent_l) - 1 if len(parent_l) < len(parent_r) else len(parent_r) - 1)
     res = parent_l[0 : index] if random.randint(0, 1) else parent_l[index : len(parent_l)] + parent_r[0 : index] if random.randint(0, 1) else parent_r[index : len(parent_r)]
 
@@ -51,16 +52,15 @@ def crossover(parent_l, parent_r):
             if (i == j):
                 count += 1
         if count >= 2:
-            return [1, 1, 1, 1]
-                
-    return res
+            return [1, 1, 1, 1]     ## assigned res to make this evolution false if find repeated bag
+    return res 
 
 def mutation(kid, times):
     for _ in range(times):
         while(True):
             mutation_index = random.randint(0, 99)
             if (mutation_index not in kid):
-                kid[random.randint(0, len(kid)) - 1] = mutation_index
+                kid[random.randint(0, len(kid)) - 1] = mutation_index ## random choose index have a successful muatation if original list dosen't have it
                 break
     return kid
 
@@ -71,11 +71,9 @@ def fitness(array_num):
         sum_fit += dict[i][1] / dict[i][0]
         sum_weight += dict[i][0]
         sum_value += dict[i][1]
-        # sum_fit = sum_value
     # sum_ = [sum_ += dict[j][0] for i in array for j in population[i]]
     if sum_weight < 285 :
-        # print(sum_fit, (tol_weight - sum_weight) / tol_weight, "fitness", sum_fit * (tol_weight - sum_weight) / tol_weight,)
-        return sum_fit * sum_value
+        return sum_fit * sum_value ## fitness fuc is value * value / weight
     else:
         print(f'{sum_weight} too heavy, jump it')
         return -1
@@ -83,9 +81,9 @@ def fitness(array_num):
 def binary_tournament():
     while (True):
         i = random.randint(0, len(population) - 1)
-        j = random.randint(0, len(population) - 1)
+        j = random.randint(0, len(population) - 1) 
         if i != j: break
-    return i if fitness(population[i]) > fitness(population[j]) else j
+    return i if fitness(population[i]) > fitness(population[j]) else j ## random choose the best parent into two players
 
 import sys
 sys.setrecursionlimit(11000)
