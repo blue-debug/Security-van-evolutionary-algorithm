3246e65b3dda90a1df4bb98a1ea3549de3e1b6a3
blue-debug <blueybf777@outlook.com>
2022-12-08 06:52:10 +0800

    1st adjust return value to correct

--- security/blpcheck.py
+++ security/blpcheck.py
@@ -152,53 +152,65 @@ for c in cat:
 	if (c == 'A' or c == 'B' or c =='C'):
 		charliecurrentcat.append(c)
 
-#MAIN
 def domfunction(o, s, name, function):
     # print(o,s,name,function)
-    for index,value in enumerate(o):
+    for index, value in enumerate(o):
         if value == '1':
             docv = 'A'
             if docv in s:
-                print(name,'',function,' is pass')
+                #print(name,'',function,' is pass')
+                pass
             docv = 'B'
             if docv in s:
-                print(name,'',function,' is pass')    
+                # rint(name,'',function,' is pass')    
+                pass
             else :
-                print(name,'',function,' is failed')
+                # print(name,'',function,' is failed')
                 return False
         elif value == '2':
             docv = 'C'
             if docv in s:
-                print(name,'',function,' is pass')
+                # print(name,'',function,' is pass')
+                pass
             else :
-                print(name,'',function,' is failed')
+                # print(name,'',function,' is failed')
                 return False
         elif value == '3':
             docv = 'B'
             if docv in s:
-                print(name,'',function,' is pass')
+                # print(name,'',function,' is pass')
+                pass
             else :
-                print(name,'',function,' is failed')
+                # print(name,'',function,' is failed')
                 return False
+    print('true')
+    return True
+
+docs_security_levels = {"1": {'A': 'l', 'B': 'l', 'C': 'h'}, "2": {'A': 'l', 'B': 'l', 'C': 'h'},
+                        "3": {'A': 'l', 'B': 'h', 'C': 'l'}}
+doc1_security_levels = {'l': ['A', 'B']}
+doc2_security_levels = {'h': ['C']}
+doc3_security_levels = {'h': ['B']}
     
 def ssc(alice, bob, charlie):
     circle = [alice, bob, charlie]
     circle_name = ['alice', 'bob', 'charlie']
     # circle = [{'alice', alice}, {'bob', bob}, {'charlice',charlie}]
     for i in range(len(circle)):
+        print(i)
         obj = circle[i]
-        print(i, obj)
+        # print(i, obj)
         if len(obj):
-            print(f'{obj} key {next(iter(obj))} value, {next(iter(obj.values()))}')
+            # print(f'{obj} key {next(iter(obj))} value, {next(iter(obj.values()))}')
             doc = next(iter(obj))
             rights = next(iter(obj.values()))
             if rights == 'w' or rights == 'r':
-                print(f'{circle_name[i]} ssc check is starting')
+                # print(f'{circle_name[i]} ssc check is starting')
                 res = domfunction(doc, alicemaxcat if circle_name[i] == 'alice' else bobmaxcat if circle_name[i] == 'bob' else charliemaxcat, circle_name[i], 'ssc')
                 if not res: 
                     print('fail')
-                    return True
-    return False
+                    return False
+    return True
 
 def star(alice, bob, charlie):
     circle = [alice, bob, charlie]
@@ -210,8 +222,8 @@ def star(alice, bob, charlie):
             rights = next(iter(obj.values()))
             if rights == 'a' or rights == 'r' or rights == 'w':
                 res = domfunction(doc, alicecurrentcat if circle_name[i] == 'alice' else bobcurrentcat if circle_name[i] == 'bob' else charliecurrentcat, circle_name[i], 'star')
-                if not res: return True
-    return False
+                if not res: return False
+    return True
 
 def ds(alice, bob, charlie):
     circle = [alice, bob, charlie]
@@ -220,23 +232,10 @@ def ds(alice, bob, charlie):
         obj = circle[i]
         if len(obj):
             rights = next(iter(obj.values()))
-            if list((rightsalice if circle_name[i] == 'alice' else rightsbob if circle_name[i] == 'bob' else rightscharlie).values()).count(rights) > 0:
-                print(f'{circle_name[i]} ds check passed')
+            if list((rightsalice if circle_name[i] == 'alice' else rightsbob if circle_name[i] == 'bob' else rightscharlie).values()).count(rights) == 0:
+                return False
+                # print(f'{circle_name[i]} ds check passed')
     return True
-
-    # if len(alice) !=0:	 
-    #     alice_rights = next(iter(alice.values()))
-    #     print(rightsalice, "alice", alice_rights)
-    #     if list(rightsalice.values()).count(alice_rights) > 0:
-    #         print('Alice ds check is pass')
-    # if len(bob) !=0:	 
-    #     bob_rights = next(iter(bob.values()))
-    #     if list(rightsbob).count(bob_rights) > 0:
-    #         print('Bob ds check is pass')
-    # if len(charlie) !=0:	 
-    #     charlis_rights = next(iter(charlie.values()))
-    #     if list(rightscharlie).count(charlis_rights) > 0:
-    #         print('charlie ds check is pass')
 
 alice = {}
 bob = {}
